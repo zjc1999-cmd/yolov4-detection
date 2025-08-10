8ec973f929ed1cafc8f3f134fcc75647b666226c
zjc1999-cmd <zjc19990215@gmail.com>
2025-08-10 14:08:16 +0900

    Update utils_fit.py

--- utils_fit.py
+++ utils_fit.py
@@ -23,50 +23,36 @@ def fit_one_epoch(model_train, model, yolo_loss, loss_history, eval_callback, op
             if cuda:
                 images  = images.cuda(local_rank)
                 targets = [ann.cuda(local_rank) for ann in targets]
-        #----------------------#
-        #   清零梯度
-        #----------------------#
+        
         optimizer.zero_grad()
         if not fp16:
-            #----------------------#
-            #   前向传播
-            #----------------------#
+           
             outputs         = model_train(images)
 
             loss_value_all  = 0
-            #----------------------#
-            #   计算损失
-            #----------------------#
+            
             for l in range(len(outputs)):
                 loss_item = yolo_loss(l, outputs[l], targets)
                 loss_value_all  += loss_item
             loss_value = loss_value_all
 
-            #----------------------#
-            #   反向传播
-            #----------------------#
+            
             loss_value.backward()
             optimizer.step()
         else:
             from torch.cuda.amp import autocast
             with autocast():
-                #----------------------#
-                #   前向传播
-                #----------------------#
+               
                 outputs         = model_train(images)
 
                 loss_value_all  = 0
-                #----------------------#
-                #   计算损失
-                #----------------------#
+                
                 for l in range(len(outputs)):
                     loss_item = yolo_loss(l, outputs[l], targets)
                     loss_value_all  += loss_item
                 loss_value = loss_value_all
 
-            #----------------------#
-            #   反向传播
-            #----------------------#
+            
             scaler.scale(loss_value).backward()
             scaler.step(optimizer)
             scaler.update()
@@ -93,19 +79,13 @@ def fit_one_epoch(model_train, model, yolo_loss, loss_history, eval_callback, op
             if cuda:
                 images  = images.cuda(local_rank)
                 targets = [ann.cuda(local_rank) for ann in targets]
-            #----------------------#
-            #   清零梯度
-            #----------------------#
+            
             optimizer.zero_grad()
-            #----------------------#
-            #   前向传播
-            #----------------------#
+            
             outputs         = model_train(images)
 
             loss_value_all  = 0
-            #----------------------#
-            #   计算损失
-            #----------------------#
+            
             for l in range(len(outputs)):
                 loss_item = yolo_loss(l, outputs[l], targets)
                 loss_value_all  += loss_item
@@ -124,9 +104,7 @@ def fit_one_epoch(model_train, model, yolo_loss, loss_history, eval_callback, op
         print('Epoch:'+ str(epoch + 1) + '/' + str(Epoch))
         print('Total Loss: %.3f || Val Loss: %.3f ' % (loss / epoch_step, val_loss / epoch_step_val))
         
-        #-----------------------------------------------#
-        #   保存权值
-        #-----------------------------------------------#
+        
         if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
             torch.save(model.state_dict(), os.path.join(save_dir, "ep%03d-loss%.3f-val_loss%.3f.pth" % (epoch + 1, loss / epoch_step, val_loss / epoch_step_val)))
             
@@ -134,4 +112,5 @@ def fit_one_epoch(model_train, model, yolo_loss, loss_history, eval_callback, op
             print('Save best model to best_epoch_weights.pth')
             torch.save(model.state_dict(), os.path.join(save_dir, "best_epoch_weights.pth"))
             
+
         torch.save(model.state_dict(), os.path.join(save_dir, "last_epoch_weights.pth"))
