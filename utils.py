aeb3e6b088d666ba613facd5ef724ce4b91d0956
zjc1999-cmd <zjc19990215@gmail.com>
2025-08-10 14:01:23 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,10 +4,7 @@ import numpy as np
 import torch
 from PIL import Image
 
-#---------------------------------------------------------#
-#   将图像转换成RGB图像，防止灰度图在预测时报错。
-#   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-#---------------------------------------------------------#
+
 def cvtColor(image):
     if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
         return image 
@@ -15,9 +12,7 @@ def cvtColor(image):
         image = image.convert('RGB')
         return image 
     
-#---------------------------------------------------#
-#   对输入图像进行resize
-#---------------------------------------------------#
+
 def resize_image(image, size, letterbox_image):
     iw, ih  = image.size
     w, h    = size
@@ -33,18 +28,14 @@ def resize_image(image, size, letterbox_image):
         new_image = image.resize((w, h), Image.BICUBIC)
     return new_image
 
-#---------------------------------------------------#
-#   获得类
-#---------------------------------------------------#
+
 def get_classes(classes_path):
     with open(classes_path, encoding='utf-8') as f:
         class_names = f.readlines()
     class_names = [c.strip() for c in class_names]
     return class_names, len(class_names)
 
-#---------------------------------------------------#
-#   获得先验框
-#---------------------------------------------------#
+
 def get_anchors(anchors_path):
     '''loads the anchors from a file'''
     with open(anchors_path, encoding='utf-8') as f:
@@ -53,16 +44,12 @@ def get_anchors(anchors_path):
     anchors = np.array(anchors).reshape(-1, 2)
     return anchors, len(anchors)
 
-#---------------------------------------------------#
-#   获得学习率
-#---------------------------------------------------#
+
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
-#---------------------------------------------------#
-#   设置种子
-#---------------------------------------------------#
+
 def seed_everything(seed=11):
     random.seed(seed)
     np.random.seed(seed)
@@ -72,9 +59,7 @@ def seed_everything(seed=11):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-#---------------------------------------------------#
-#   设置Dataloader的种子
-#---------------------------------------------------#
+
 def worker_init_fn(worker_id, rank, seed):
     worker_seed = rank + seed
     random.seed(worker_seed)
